dcff001329bf54b382f1aaf7359c038f540cb59a
김주엽 <kjyy08@naver.com>
2025-04-28 22:28:02 +0900

    first commit

---
+++ src/main/java/org/lucky0111/pettalk/PetTalkApplication.java
@@ -0,0 +1,13 @@
+package org.lucky0111.pettalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lucky0111/pettalk/PetTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lucky0111.pettalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
