049efac1ea9c78c1d6ff9545b79e67bd3ca95aad
lSNOTNULL <iream1@naver.com>
2025-05-06 22:17:24 +0900

    feat:훈련사#8 클린코드 + userRole Enum추가

--- src/main/java/org/lucky0111/pettalk/exception/CustomException.java
+++ src/main/java/org/lucky0111/pettalk/exception/CustomException.java
@@ -4,23 +4,32 @@ import lombok.Getter;
 import org.lucky0111.pettalk.domain.common.ErrorCode;
 import org.springframework.http.HttpStatus;
 
+import java.io.IOException;
+
 @Getter
 public class CustomException extends RuntimeException {
-    private final String message;
+//    private final String message;
     private final HttpStatus httpStatus;
 
     public CustomException(String message, HttpStatus httpStatus) {
-        this.message = message;
+        super(message);
         this.httpStatus = httpStatus;
     }
 
     public CustomException(ErrorCode errorCode) {
-        this.message = errorCode.getMessage();
+        super(errorCode.getMessage());
         this.httpStatus = errorCode.getHttpStatus();
     }
 
     public CustomException(ErrorCode errorCode, String additionalInfo) {
-        this.message = errorCode.getMessage() + ": " + additionalInfo;
+        super(errorCode.getMessage() + ": " + additionalInfo);
         this.httpStatus = errorCode.getHttpStatus();
+    }
+    public CustomException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+    }
+    public HttpStatus getStatus() {
+        return httpStatus;
     }
 }
