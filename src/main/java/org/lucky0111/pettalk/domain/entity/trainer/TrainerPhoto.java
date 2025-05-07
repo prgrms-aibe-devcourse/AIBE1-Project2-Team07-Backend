ed159633fffa900496679ceffc090f61359bbbe4
lSNOTNULL <iream1@naver.com>
2025-05-07 20:10:10 +0900

    feat: 훈련사 Photo, ServiceFee 추가

---
+++ src/main/java/org/lucky0111/pettalk/domain/entity/trainer/TrainerPhoto.java
@@ -0,0 +1,28 @@
+package org.lucky0111.pettalk.domain.entity.trainer;
+
+import jakarta.persistence.*;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.lucky0111.pettalk.domain.common.BaseTimeEntity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name ="trainer_photos")
+public class TrainerPhoto extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trainer_id")
+    private Trainer trainer;
+
+    @Column(length = 1000)
+    private String fileUrl;
+    private Integer photoOrder; // 사진 표시 순서 (예: 1, 2)
+}
---
+++ src/main/java/org/lucky0111/pettalk/repository/trainer/TrainerServiceFeeRepository.java
@@ -0,0 +1,7 @@
+package org.lucky0111.pettalk.repository.trainer;
+
+import org.lucky0111.pettalk.domain.entity.trainer.TrainerServiceFee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainerServiceFeeRepository extends JpaRepository<TrainerServiceFee, Long> {
+}
