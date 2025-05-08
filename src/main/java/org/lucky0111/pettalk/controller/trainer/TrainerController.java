fa943b60739900dbd14b8b36d556bca0eb13a5a6
lSNOTNULL <iream1@naver.com>
2025-05-09 00:33:33 +0900

    docs: swagger Operation 추가

--- src/main/java/org/lucky0111/pettalk/controller/trainer/TrainerController.java
+++ src/main/java/org/lucky0111/pettalk/controller/trainer/TrainerController.java
@@ -3,13 +3,10 @@ package org.lucky0111.pettalk.controller.trainer;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.lucky0111.pettalk.domain.dto.community.PostResponseDTO;
 import org.lucky0111.pettalk.domain.dto.auth.CustomOAuth2User;
 import org.lucky0111.pettalk.domain.dto.trainer.CertificationRequestDTO;
-import org.lucky0111.pettalk.domain.dto.trainer.TrainerApplicationRequestDTO;
 import org.lucky0111.pettalk.domain.dto.trainer.TrainerDTO;
 import org.lucky0111.pettalk.domain.dto.trainer.TrainerPageDTO;
-import org.lucky0111.pettalk.domain.entity.user.PetUser;
 import org.lucky0111.pettalk.service.trainer.TrainerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,12 +34,14 @@ public class TrainerController {
     }
 
     @GetMapping("/{trainerNickname}") // 조회만 Nickname을 이용해서.
+    @Operation(summary = "트레이너 전체 내용 조회", description = "Nickname으로 전체 내용을 조회합니다.")
     public ResponseEntity<TrainerDTO> getTrainer(@PathVariable String trainerNickname) {
         TrainerDTO dto = trainerService.getTrainerDetails(trainerNickname);
         return ResponseEntity.ok().body(dto);
     }
 
     @PostMapping("/apply")
+    @Operation(summary = "트레이너 승급 신청", description = "승급 신청 시 자격증 정보 필요하며, 파일은 multipart/form-data 필요")
     public ResponseEntity<Void> applyForTrainer(
             @AuthenticationPrincipal CustomOAuth2User principal,
             @RequestPart("certification") @Valid CertificationRequestDTO certificationDTO,
@@ -55,6 +54,7 @@ public class TrainerController {
     }
 
     @PostMapping("/{trainerId}/certifications")
+    @Operation(summary = "트레이너 자격증 추가", description = "승급 된 이후 자격증을 더 추가할때 사용하며, 파일은 multipart/form-data 필요")
     public ResponseEntity<Void> addTrainerCertification(
             @PathVariable UUID trainerId, // 경로 변수에서 트레이너 ID 수신 (신청 시작 시 부여받은/생성된 ID)
             @RequestPart("certification") @Valid CertificationRequestDTO certificationDTO,
