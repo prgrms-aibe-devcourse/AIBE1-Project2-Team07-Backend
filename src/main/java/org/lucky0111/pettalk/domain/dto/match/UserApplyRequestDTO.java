c38b339b5270bc5830f6f3b3a89c1cbe88103069
soheeGit <63wlsthgml@gmail.com>
2025-05-08 17:55:08 +0900

    리펙토링

--- src/main/java/org/lucky0111/pettalk/domain/dto/match/UserApplyRequestDTO.java
+++ src/main/java/org/lucky0111/pettalk/domain/dto/match/UserApplyRequestDTO.java
@@ -3,12 +3,11 @@ package org.lucky0111.pettalk.domain.dto.match;
 import org.lucky0111.pettalk.domain.common.ServiceType;
 
 public record UserApplyRequestDTO(
-        String trainerName,
+        String trainerNickName,
         ServiceType serviceType,
         String petType,
         String petBreed,
         Integer petMonthAge,
-        String content,
-        String imageUrl
+        String content
 ) {
 }
