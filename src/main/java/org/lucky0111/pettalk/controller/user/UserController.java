06ca597b26fde2199b731bcb67ad9eaa628f1ce0
rbxo0128 <rbxo0128@naver.com>
2025-05-09 12:40:26 +0900

    feat: 프로필 수정 api 추가

--- src/main/java/org/lucky0111/pettalk/controller/user/UserController.java
+++ src/main/java/org/lucky0111/pettalk/controller/user/UserController.java
@@ -2,11 +2,13 @@ package org.lucky0111.pettalk.controller.user;
 
 import lombok.RequiredArgsConstructor;
 import org.lucky0111.pettalk.domain.dto.user.UserResponseDTO;
+import org.lucky0111.pettalk.domain.dto.user.UserUpdateDTO;
 import org.lucky0111.pettalk.service.user.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -18,5 +20,21 @@ public class UserController {
     public ResponseEntity<?> getUser() {
         UserResponseDTO userResponseDTO = userService.getUserById();
         return ResponseEntity.ok(userResponseDTO);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<UserUpdateDTO> updateUser(
+            @RequestBody UserUpdateDTO requestDTO
+    ) {
+        UserUpdateDTO userUpdateDTO = userService.updateUser(requestDTO);
+        return ResponseEntity.ok(userUpdateDTO);
+    }
+
+    @PutMapping("/updateImage")
+    public ResponseEntity<UserUpdateDTO> updateUserImage(
+            @RequestParam("file") MultipartFile image
+    ) throws IOException {
+        UserUpdateDTO userUpdateDTO = userService.updateUserImage(image);
+        return ResponseEntity.ok(userUpdateDTO);
     }
 }
--- src/main/java/org/lucky0111/pettalk/domain/dto/user/ProfileUpdateDTO.java
+++
@@ -1,5 +0,0 @@
-package org.lucky0111.pettalk.domain.dto.user;
-
-public record ProfileUpdateDTO(String nickname, String profileImageUrl) {
-
-}
