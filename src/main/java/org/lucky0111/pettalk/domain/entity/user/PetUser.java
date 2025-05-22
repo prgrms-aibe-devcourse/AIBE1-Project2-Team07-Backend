cf4c3f2cf107b39fe1f3c5c99ea3a5fb31a75ff2
김주엽 <kjyy08@naver.com>
2025-05-22 18:32:30 +0900

    refactor: 유저 엔티티 setter 개선

--- src/main/java/org/lucky0111/pettalk/domain/entity/user/PetUser.java
+++ src/main/java/org/lucky0111/pettalk/domain/entity/user/PetUser.java
@@ -1,16 +1,14 @@
 package org.lucky0111.pettalk.domain.entity.user;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.lucky0111.pettalk.domain.common.BaseTimeEntity;
 import org.lucky0111.pettalk.domain.common.UserRole;
 
 import java.util.UUID;
 
-
-@Setter
 @Getter
 @Entity
 @Table(name = "pet_users")
@@ -38,6 +36,40 @@ public class PetUser extends BaseTimeEntity {
     private String socialId; // 토큰 > 소셜 ID
 
     private String status;
+
+    @Builder
+    public PetUser(String name, String email, String nickname, String profileImageUrl, String provider, String socialId) {
+        this.name = name;
+        this.email = email;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.provider = provider;
+        this.socialId = socialId;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updateRole(UserRole role) {
+        this.role = role;
+    }
+
+    public void updateStatus(String status) {
+        this.status = status;
+    }
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void updateProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public void updateSocialId(String socialId) {
+        this.socialId = socialId;
+    }
 }
 
 
