2db144af17b618f0fed8aa49facd177d7dd9b873
김주엽 <kjyy08@naver.com>
2025-06-05 19:54:34 +0900

    refactor: 불필요한 쿼리 메서드 제거

--- src/main/java/org/lucky0111/pettalk/repository/user/PetUserRepository.java
+++ src/main/java/org/lucky0111/pettalk/repository/user/PetUserRepository.java
@@ -2,18 +2,14 @@ package org.lucky0111.pettalk.repository.user;
 
 import org.lucky0111.pettalk.domain.entity.user.PetUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface PetUserRepository extends JpaRepository<PetUser, UUID> {
-    Optional<PetUser> findByEmail(String email);
-
-    Optional<PetUser> findByNickname(String nickname);
-
     Optional<PetUser> findBySocialId(String socialId);
-
-    PetUser findByProviderAndSocialId(String provider, String socialId);
 
     boolean existsByNickname(String nickname);
 }
