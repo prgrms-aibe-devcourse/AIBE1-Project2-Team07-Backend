e712b40eadc38e0cef958838fa72c92944f60971
김주엽 <kjyy08@naver.com>
2025-06-07 23:43:12 +0900

    refactor: 리포지토리 어노테이션 제거

--- src/main/java/org/lucky0111/pettalk/repository/user/PetUserRepository.java
+++ src/main/java/org/lucky0111/pettalk/repository/user/PetUserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface PetUserRepository extends JpaRepository<PetUser, UUID> {
     Optional<PetUser> findBySocialId(String socialId);
 
