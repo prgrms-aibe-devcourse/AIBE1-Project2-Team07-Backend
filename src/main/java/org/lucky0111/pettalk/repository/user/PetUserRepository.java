47cd1efc436c7d6611846d80657503bcc72c056a
김주엽 <kjyy08@naver.com>
2025-06-07 23:45:12 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/org/lucky0111/pettalk/repository/user/PetUserRepository.java
+++ src/main/java/org/lucky0111/pettalk/repository/user/PetUserRepository.java
@@ -2,7 +2,6 @@ package org.lucky0111.pettalk.repository.user;
 
 import org.lucky0111.pettalk.domain.entity.user.PetUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
